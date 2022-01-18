6f4c3d684b34cf867b1b309315381fb5a5636627
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-18 08:32:21 +0530

    finished adding models

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,15 @@ from distutils.command.upload import upload
 from email.policy import default
 from turtle import title
 from unicodedata import category
+import uuid
 from django.db import models
+from datetime import datetime, timedelta
+
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 # Create your models here.
 class Categories(models.Model) :
@@ -14,15 +22,15 @@ class Categories(models.Model) :
 
 
 class Attributes(models.Model) :
-    type = models.CharField(max_length=100)
-    value = models.IntegerChoices
+    type = models.CharField(max_length=100, unique=True)
+    value = models.IntegerChoices()
     description = models.CharField(max_length=155)
 
     def __str__(self) -> str:
         return self.type
 
 class Brand(models.Model) :
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self) -> str:
         return self.name
@@ -38,7 +46,23 @@ class Product(models.Model) :
     category = models.ManyToManyField(Categories, on_delete=models.RESTRICT)
     brand = models.ForeignKey(Brand, on_delete=models.RESTRICT)
 
-
-
     def __str__(self) -> str:
         return self.title
+
+
+class Stock(models.Model) :
+    stock = models.IntegerField()
+    sold = models.IntegerField()
+    debit = models.DateTimeField(default=datetime.utcnow()+timedelta(hours=5.5))
+    credit = models.DateTimeField(default=datetime.utcnow()+timedelta(hours=5.5))
+
+
+class ProductInventory(models.Model) :
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    uuid = models.UUIDField(unique=True)
+    attributes = models.ManyToManyField(blank=True, null=True)
+    price = models.IntegerField()
+    stock = models.OneToOneField(Stock, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.uuid
