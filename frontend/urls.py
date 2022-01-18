856c02ba9abf663bae4e00e62372932c27582dab
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-18 20:42:21 +0530

    categories

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .  import views
 
+app_name = "frontend"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name =  'frontend/index.html') ),
+    path("categories/", views.categories , name = "list-categories" )
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from inventory.models import Categories
+from django.db.models import Count
 
 # Create your views here.
+def categories(request) :
+    data = Categories.objects.all().annotate(
+    product_count=Count('product'))
+
+    return render(request, "frontend/categories.html", {"data": data})
