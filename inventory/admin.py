519b660938e658f25c9bb242af23a12707e5a83a
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-18 08:48:31 +0530

    admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categories, Product, ProductInventory, Brand, Attributes, Stock
 
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(Product)
+admin.site.register(ProductInventory)
+admin.site.register(Brand)
+admin.site.register(Attributes)
+admin.site.register(Stock)
