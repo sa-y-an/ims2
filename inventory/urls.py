2346289321d1b10bdfbe75607fdd718a611853ea
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-19 19:26:06 +0530

    added categories

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,19 @@
 from os import name
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .  import views
+from .views  import *
 
 
 app_name = "inventory"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name =  'inventory/index.html') , name="index"),
-    path('category-list', views.categorylist)
+
+    # category
+    path('category-list', CategoryList.as_view(template_name="inventory/category-list.html"), name="cat-list"),
+    path('category/add/', CategoryCreateView.as_view(template_name = "inventory/createview.html"), name='cat-add'),
+    path('category/<int:pk>/', CategoryUpdateView.as_view(template_name = "inventory/createview.html"), name='cat-update'),
+    path('category/<int:pk>/delete/', CategoryDeleteView.as_view(template_name = "inventory/deleteview.html"), name='cat-delete'),
+
+    # product
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import *
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+
+# category views 
+
+class CategoryList(ListView) :
+    model = Categories
+
+
+class CategoryCreateView(CreateView):
+    model = Categories
+    fields = '__all__'
+    success_url = reverse_lazy('inventory:cat-list')
+
+class CategoryCreateView(CreateView):
+    model = Categories
+    fields = '__all__'
+    success_url = reverse_lazy('inventory:cat-list')
+
+
+class PostCreateView(CreateView):
+    model = Product
+    fields = []
+
+class CategoryUpdateView(UpdateView):
+    model = Categories
+    fields = '__all__'
+    success_url = reverse_lazy('inventory:cat-list')
+
+class CategoryDeleteView(DeleteView):
+    model = Categories
+    success_url = reverse_lazy('inventory:cat-list')
