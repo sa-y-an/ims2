8cf1aa0de98c04bcb2094655bb949412942ef3bc
sa-y-an <sayan.ee.nitd@gmail.com>
2022-01-19 15:17:46 +0530

    some css styling

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%x#rty2d#5-q04&#ce74pbt#6jq=d5rluh7qzeqkvd%$dw0)yd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
